3ff147ea4aaf0b2f8038b715e6acf62d2da47ff8
Dmitry Sazonov <sazon@nxt.ru>
2014-08-24 00:04:58 +0400

    Copying File & Autoclose

---
+++ AutoCloseCopyFile.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class AutoCloseCopyFile
+{
+  public static void main(String[] args) throws IOException
+  {
+    int i; 
+
+    if(args.length != 2) {
+    	System.out.println("Usage: CopyFile from to");
+    	return;
+    }
+
+    try(FileInputStream fin = new FileInputStream(args[0]);
+        FileOutputStream fout = new FileOutputStream(args[1]))
+    {
+    	do {
+    		i = fin.read();
+    		if(i != -1) fout.write(i);
+    	} while(i != -1);
+  	} catch(IOException exc) {
+  		System.out.println("I/O Error: " + exc);
+  	}
+  }
+}
